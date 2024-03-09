1b8d705bd61b30fcfd42917a64c5209382ebeb1d
anoopghm <140532297+anoopghm@users.noreply.github.com>
2024-03-09 19:36:59 +0530

    Update App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -25,3 +25,31 @@ function App() {
 }
 
 export default App;
+
+// same code in the form of arror function 
+//     import React from "react";
+// import Entry from "./Entry";
+// import emojipedia from "../emojipedia";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>
+//         <span>emojipedia</span>
+//       </h1>
+//       <dl className="dictionary">{emojipedia.map( emojiTerm => (
+//                           <Entry
+//                           key={emojiTerm.id}
+//                           emoji={emojiTerm.emoji}
+//                           name={emojiTerm.name}
+//                           description={emojiTerm.meaning}
+//                         />
+//                               );
+//                          )}
+//       </dl> 
+//     </div>
+//   );
+// }
+
+// export default App;
+
